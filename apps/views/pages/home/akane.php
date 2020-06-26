bec42a96efc9a81a315ffd364f315801883b835d
Bagas Ranggen <bagas@logique.co.id>
2020-06-26 15:27:55 +0700

    update layout on akane and momiji pages

--- apps/views/pages/home/akane.php
+++ apps/views/pages/home/akane.php
@@ -27,42 +27,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div class="container">
   <div style="margin: 20px 0;">
-      <ul class="breadcrumbdesign">
-          <li><?php echo '<a href="'.base_url().'">Beranda</a>' ?></li>
-          <li><a href="#">Rumah</a></li>
-      </ul>
-    </div>
+    <ul class="breadcrumbdesign">
+        <li><?php echo '<a href="'.base_url().'">Beranda</a>' ?></li>
+        <li><a href="#">Rumah</a></li>
+    </ul>
+  </div>
   <div style="margin: 40px 0;">
     <p class="home-desc">
     Green Bamboo Terrace Bogor terletak di Kawasan seluas 1,7 Ha dengan harga mulai dari Rp. 1 M-an untuk setiap unitnya. Kami memiliki jumlah unit yang sangat terbatas menjadikan
       <b>Green Bamboo Terrace Bogor sangat exclusive dan private.</b>
     </p>
+    <br />
+    <p class="home-desc"><b>Tipe Akane</b>
+    Tipe Akane adalah rumah minimalis modern 2 lantai tipe 95 dengan 3 kamar tidur yang dibangun dengan standar baru untuk hunian yang aman, nyaman dan asri. Rumah ini dirancang sedemikian rupa sehingga seluruh ruangannya bisa mendapatkan cahaya alami dan ventilasi yang baik.</p>
   </div>
-  <ul class="nav nav-tabs green-tabs" role="tablist">
-    <li class="nav-item text-center">
-      <a class="nav-link akane-tab active" data-toggle="tab" role="tab" aria-controls="type-akane" href="#type-akane" aria-selected="true">Type Akane</a>
-    </li>
-  </ul>
-  <div class="tab-content type-home">
-    <div id="type-akane" class="tab-pane fade show active" role="tabpanel" aria-labelledby="akane-tab">
-      <div class="row">
-        <div class="col-md-8 col-xl-9">
-          <img class="img-fluid" src="<?php echo base_url();?>assets/images/the-homes/type-akane.png" alt="Rumah Type 90 di Bogor">
-        </div>
-        <div class="col-md-4 col-xl-3">
-          <div class="side-info-showcase">
-            <div class="list-info">
-              <img src="<?php echo base_url();?>assets/images/icon/house_outline.png" alt="Luas">
-              LB : 95,5 M
+
+  <section class="type__header">
+    <div class="row align-items-center">
+      <div class="col-md-3"><h2 class="head-title">TIPE AKANE</h2></div>
+      <div class="col-md-3 col-xl-5"><hr></div>
+      <div class="col-md-6 col-xl-4"><a href="<?php echo base_url ('the-homes/momiji'); ?>" class="form-control btn">Lihat Juga Tipe Momiji &nbsp; <span>&rarr;</span></a></div>
+    </div>
+  </section>
+
+  <section class="type">
+    <div id="type-akane">
+
+      <div class="type__summary" >
+        <div class="row">
+          <div class="col-md-12">
+            <img class="img-fluid summary__img" src="<?php echo base_url();?>assets/images/the-homes/type-akane.png" alt="Rumah Type 90 di Bogor">
+          </div>
+          <div class="col-md-12">
+            <div class="side-info-showcase">
+              <div class="list-info">
+                <img src="<?php echo base_url();?>assets/images/icon/house_outline.png" alt="Luas">
+                LB : 95,5 M
+              </div>
+              <div class="list-info">
+                <img src="<?php echo base_url();?>assets/images/icon/double_king_size_bed.png" alt="Kamar">
+                3 Kamar Tidur
+              </div>
+              <a class="link-denah" data-toggle="modal" data-target="#akane-modal" href="#">Lihat denah Bangunan</a>
             </div>
-            <div class="list-info">
-              <img src="<?php echo base_url();?>assets/images/icon/double_king_size_bed.png" alt="Kamar">
-              3 Kamar Tidur
-            </div>
-            <a class="link-denah" data-toggle="modal" data-target="#akane-modal" href="#">Lihat denah Bangunan</a>
           </div>
         </div>
       </div>
+
       <div class="title-section-line break-section">
         <div class="mr-md-4">
           <div class="jumbotron__category">
@@ -73,6 +84,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <hr>
         </div>
       </div>
+
       <div class="the-homes-showcase">
         <div class="row mx-0">
           <div class="col-12 col-sm-7 col-md-8 col-xl-9 px-0 pr-sm-2 text-center">
@@ -107,8 +119,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           </div>
         </div>
       </div>
+
+      <div class="row text-center mt-4 mt-sm-0">
+        <div class="col-md-12"><a href="<?php echo base_url ('the-homes/momiji'); ?>" class="form-control btn type__btn">Lihat Juga Tipe Momiji</a></div>
+      </div>
+
     </div>
-  </div>
+  </section>
+
   <div class="title-section-line break-section">
     <div class="mr-md-4">
       <div class="jumbotron__category">
@@ -119,118 +137,130 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <hr>
     </div>
   </div>
+
   <div class="detail-materials">
     <img src="<?php echo base_url();?>assets/images/the-homes/detail-materials.png" alt="Material Perumahan Green Bamboo Terrace Bogor">
   </div>
-  <div class="row justify-content-center">
-      <div class="col-sm-6">
-          <table class="table table-bordered">
-              <tr>
-                  <th width="30%">Spesifikasi</th>
-                  <th>Keterangan</th>
-              </tr>
-              <tr>
-                  <td>Pondasi</td>
-                  <td>Strauss pile & beton bertulang pondasi batu kali</td>
-              </tr>
-              <tr>
-                  <td>Dinding</td>
-                  <td>Pasangan bata merah dan mortar</td>
-              </tr>
-              <tr>
-                  <td>Facade</td>
-                  <td>Batu hias paras jogja, ornament dan grillwood</td>
-              </tr>
-              <tr>
-                  <td>Finishing cat</td>
-                  <td>Kansai weathershield (exterior) & Kansai paint (interior)</td>
-              </tr>
-              <tr>
-                  <td>Finishing lantai</td>
-                  <td>
-                  -	Carport (ceramic tile Sandi Mas 40 x40) <br />
-                  -	Keramik dalam rumah HomogeneousTile Double Layer Alice 60 x 60
-                  </td>
-              </tr>
-              <tr>
-                  <td>Ruang Mandi/Toilet</td>
-                  <td>Lantai & dinding (ceramic tile roman)</td>
-              </tr>
-              <tr>
-                  <td>Struktur atas</td>
-                  <td>Beton K300</td>
-              </tr>
-              <tr>
-                  <td>Atap</td>
-                  <td>Dak beton, waterproof sika dan system insulasi</td>
-              </tr>
-              <tr>
-                  <td>Plafond</td>
-                  <td>Interior (gypsum Jaya board/Yoshino board)</td>
-              </tr>
-              <tr>
-                  <td>Tangki air</td>
-                  <td>Ground water tank & roof tank Penguin</td>
-              </tr>
-              <tr>
-                  <td>Pompa air</td>
-                  <td>Shimizu</td>
-              </tr>
-              <tr>
-                  <td>Septictank</td>
-                  <td>Bio tank</td>
-              </tr>
-              <tr>
-                  <td>Supply air</td>
-                  <td>PDAM</td>
-              </tr>
-              <tr>
-                  <td>Listrik</td>
-                  <td>Jaringan listrik bawah tanah</td>
-              </tr>
-          </table>
-      </div>
-      <div class="col-sm-6">
-          <table class="table table-bordered">
-              <tr>
-                  <th width="30%">Spesifikasi</th>
-                  <th>Keterangan</th>
-              </tr>
-              <tr>
-                  <td>Kusen pintu dan jendela</td>
-                  <td><img src="<?php echo base_url();?>assets/images/the-homes/ykk.png" alt="ykk"></td>
-              </tr>
-              <tr>
-                  <td>Pintu</td>
-                  <td><img src="<?php echo base_url();?>assets/images/the-homes/daiken.png" alt="daiken"></td>
-              </tr>
-              <tr>
-                  <td>Sanitary</td>
-                  <td><img src="<?php echo base_url();?>assets/images/the-homes/toto.png" alt="toto"></td>
-              </tr>
-              <tr>
-                  <td>Listrik</td>
-                  <td>2200VA</td>
-              </tr>
-              <tr>
-                  <td>AC</td>
-                  <td><img src="<?php echo base_url();?>assets/images/the-homes/mitsubishi.png" alt="mitsubishi"></td>
-              </tr>
-              <tr>
-                  <td>Saklar dan Power Outlet</td>
-                  <td><img src="<?php echo base_url();?>assets/images/the-homes/panasonic.png" alt="panasonic"></td>
-              </tr>
-              <tr>
-                  <td>Instalasi air panas</td>
-                  <td>Pipa westpex</td>
-              </tr>
-              <tr>
-                  <td>Filter air</td>
-                  <td><img src="<?php echo base_url();?>assets/images/the-homes/toclas.png" alt="toclas"></td>
-              </tr>
-          </table>
-      </div>
-  </div>
+
+  <section class="type__table">
+    <div class="row justify-content-center">
+        <div class="col-sm-6">
+            <table class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                    <th width="30%">Spesifikasi</th>
+                    <th>Keterangan</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                    <td>Pondasi</td>
+                    <td>Strauss pile & beton bertulang pondasi batu kali</td>
+                </tr>
+                <tr>
+                    <td>Dinding</td>
+                    <td>Pasangan bata merah dan mortar</td>
+                </tr>
+                <tr>
+                    <td>Facade</td>
+                    <td>Batu hias paras jogja, ornament dan grillwood</td>
+                </tr>
+                <tr>
+                    <td>Finishing cat</td>
+                    <td>Kansai weathershield (exterior) & Kansai paint (interior)</td>
+                </tr>
+                <tr>
+                    <td>Finishing lantai</td>
+                    <td>
+                    -	Carport (ceramic tile Sandi Mas 40 x40) <br />
+                    -	Keramik dalam rumah HomogeneousTile Double Layer Alice 60 x 60
+                    </td>
+                </tr>
+                <tr>
+                    <td>Ruang Mandi/Toilet</td>
+                    <td>Lantai & dinding (ceramic tile roman)</td>
+                </tr>
+                <tr>
+                    <td>Struktur atas</td>
+                    <td>Beton K300</td>
+                </tr>
+                <tr>
+                    <td>Atap</td>
+                    <td>Dak beton, waterproof sika dan system insulasi</td>
+                </tr>
+                <tr>
+                    <td>Plafond</td>
+                    <td>Interior (gypsum Jaya board/Yoshino board)</td>
+                </tr>
+                <tr>
+                    <td>Tangki air</td>
+                    <td>Ground water tank & roof tank Penguin</td>
+                </tr>
+                <tr>
+                    <td>Pompa air</td>
+                    <td>Shimizu</td>
+                </tr>
+                <tr>
+                    <td>Septictank</td>
+                    <td>Bio tank</td>
+                </tr>
+                <tr>
+                    <td>Supply air</td>
+                    <td>PDAM</td>
+                </tr>
+                <tr>
+                    <td>Listrik</td>
+                    <td>Jaringan listrik bawah tanah</td>
+                </tr>
+              </tbody>
+            </table>
+        </div>
+        <div class="col-sm-6">
+            <table class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                    <th width="50%">Spesifikasi</th>
+                    <th>Keterangan</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                    <td>Kusen pintu dan jendela</td>
+                    <td><img src="<?php echo base_url();?>assets/images/the-homes/ykk.png" alt="ykk"></td>
+                </tr>
+                <tr>
+                    <td>Pintu</td>
+                    <td><img src="<?php echo base_url();?>assets/images/the-homes/daiken.png" alt="daiken"></td>
+                </tr>
+                <tr>
+                    <td>Sanitary</td>
+                    <td><img src="<?php echo base_url();?>assets/images/the-homes/toto.png" alt="toto"></td>
+                </tr>
+                <tr>
+                    <td>Listrik</td>
+                    <td>2200VA</td>
+                </tr>
+                <tr>
+                    <td>AC</td>
+                    <td><img src="<?php echo base_url();?>assets/images/the-homes/mitsubishi.png" alt="mitsubishi"></td>
+                </tr>
+                <tr>
+                    <td>Saklar dan Power Outlet</td>
+                    <td><img src="<?php echo base_url();?>assets/images/the-homes/panasonic.png" alt="panasonic"></td>
+                </tr>
+                <tr>
+                    <td>Instalasi air panas</td>
+                    <td>Pipa westpex</td>
+                </tr>
+                <tr>
+                    <td>Filter air</td>
+                    <td><img src="<?php echo base_url();?>assets/images/the-homes/toclas.png" alt="toclas"></td>
+                </tr>
+              </tbody>
+            </table>
+        </div>
+    </div>
+  </section>
   <div class="txt-center" style="margin-bottom: 40px;">
       <a href="<?php echo base_url();?>assets/images/detail/toclas.png" data-toggle="lightbox">
         <img src="<?php echo base_url();?>assets/images/detail/toclas.png" class="w-100" alt="Filter Air Teknologi Canggih Toclas" />
