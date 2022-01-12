273e20b296a5ab1cf7d485328d03a3f9702ead18
Susanto Effendi <santoe@santoe.net>
2022-01-12 18:02:57 +0700

    Setup Tawk to

--- apps/views/layouts/main.php
+++ apps/views/layouts/main.php
@@ -741,6 +741,20 @@ $this->load->library('Layouts');
     //   }
     // });
   </script>
+
+  <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61de5853f7cf527e84d1ac93/1fp68i1tk';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 
 </html>
