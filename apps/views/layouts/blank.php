a4c92d8087f09e2a5ee695bdf7a53f10452e6e09
Calvin Octa Wijaya <calvin@logique.co.id>
2022-01-27 22:21:25 +0700

    in progress - banner and topside section

---
+++ apps/views/layouts/blank.php
@@ -0,0 +1,103 @@
+<?php
+$this->load->library('Layouts');
+?>
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Cache-Control: max-age=2592000" content="public">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo $title; ?></title>
+  <meta name="description" content="<?php echo $description; ?>">
+  <meta name="keywords" content="<?php echo $keywords; ?>">
+  <meta name="author" content="">
+  <link rel="icon" href="<?php echo base_url(); ?>assets/images/ico16.png" type="image/gif" sizes="16x16">
+  <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/ico16.png" />
+  <link rel="apple-touch-icon-precomposed" href="<?php echo base_url(); ?>assets/images/ico16.png" />
+  <link rel="canonical" href="<?= $canonical ? $canonical : "https://www.bambootajur.com" ?>">
+  <meta property="og:title" content="<?php echo $title; ?>">
+  <meta property="og:type" content="website" />
+  <meta property="og:description" content="<?php echo $description; ?>" />
+  <meta property="og:url" content="<?php echo $url ? $url : "https://www.bambootajur.com"; ?>" />
+  <meta property="og:site_name" content="Bamboo Tajur" />
+  <meta property="og:image" content="<?= base_url(); ?>assets/images/ogp-image.jpg" />
+  <meta property="og:locale:alternate" content="id_ID" />
+  <meta content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=no name=viewport">
+  <meta name="facebook-domain-verification" content="6wavtvcosv0t3yfkkyt9ybrkmp7vts" />
+  <!-- Stylesheet -->
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+  <link rel="preconnect" href="https://www.googleadservices.com" />
+  <link rel="preconnect" href="https://www.facebook.com" />
+  <link rel="preconnect" href="https://connect.facebook.net" />
+  <link rel="preconnect" href="https://www.google.com" />
+  <link rel="preconnect" href="https://www.google.co.id" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+  <link rel="preload" as="font" href="<?= base_url(); ?>assets/fonts/Roboto-Medium.ttf" crossorigin type="font/ttf" />
+  <link rel="preload" as="font" href="<?= base_url(); ?>assets/fonts/Roboto-Light.ttf" crossorigin type="font/ttf" />
+  <link rel="preload" as="font" href="<?= base_url(); ?>assets/fonts/Roboto-Bold.ttf" crossorigin type="font/ttf" />
+  <link rel="preload" as="font" href="<?= base_url(); ?>assets/fonts/Roboto-Black.ttf" crossorigin type="font/ttf" />
+  <link rel="preload" as="font" href="<?= base_url(); ?>assets/fonts/Roboto-Regular.ttf" crossorigin type="font/ttf" />
+  <link rel="preload" as="font" href="<?= base_url(); ?>assets/fonts/OpenSans-Regular.ttf" crossorigin type="font/ttf" />
+
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Roboto&display=swap" rel="stylesheet">
+
+  <!-- <link rel="preload" href="https://fonts.googleapis.com/css?family=Arvo:400,400i,700,700i&display=swap" as="style"> -->
+  <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arvo:400,400i,700,700i&display=swap"> -->
+
+  <?php echo $this->layouts->print_includes('css'); ?>
+  <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
+  <!-- End Stylesheet-->
+  <?php
+  if (http_response_code() != 404 && $_SERVER['HTTP_HOST'] == "www.bambootajur.com") {
+  ?>
+    <!-- Google Tag Manager -->
+    <script>
+      (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+          'gtm.start': new Date().getTime(),
+          event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', 'dataLayer', 'GTM-MHZQ3QG');
+      console.log("run");
+    </script>
+    <!-- End Google Tag Manager -->
+  <?php
+  }
+  ?>
+</head>
+
+
+
+<body>
+  <?php
+  if (http_response_code() != 404 && $_SERVER['HTTP_HOST'] == "www.bambootajur.com") {
+  ?>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHZQ3QG" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+  <?php
+  }
+  ?>
+
+  <?= $content ?>
+  
+  <?php echo $this->layouts->print_includes('js'); ?>
+  <script src="<?= base_url(); ?>assets/js/modernizr-webp.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.min.js"></script>
+
+</body>
+
+</html>
---
+++ apps/views/pages/promo.php
@@ -0,0 +1,128 @@
+<div>
+    Upper content
+
+    <!-- CURRENTLY WORKING ON HUBUNGI KAMI BANNER BOTTOM SECTION -->
+    <!-- THIS PAGE DO NOT USE NAVBAR (100% based on provided design) -->
+    <!-- REMOVE THIS COMMENT AFTER READING IT -->
+    <div class="center-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdaeU85nrsRjecyLiQkOVP88WLpu9hP_zehZw2KpevtpX2usg/viewform?embedded=true" width="100%" height="100%" style="min-height: 600px;">Memuat…</iframe>
+                </div>
+                <div class="col-md-6">
+                    <div class="card-slider">
+                        <div id="banner-top-page" class="carousel slide" data-ride="carousel" data-interval="7000">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="<?= base_url() ?>assets/images/detail/denah-akane.jpg" class="img-fluid" alt="">
+                                    <h4 class="carousel-title">Denah Bangunan Type Akane</h4>
+                                    <div class="carousel-subtitle">Harga mulai 1M</div>
+                                    <p class="carousel-text">Tipe Akane adalah rumah minimalis modern 2 lantai tipe 95 dengan 3 kamar tidur, dilengkapi dengan berbagai material premium dengan standar kualitas Jepang, seperti material HomogeneousTile Double Layer Alice yang digunakan sebagai penutup lantai sehingga membuat setiap injakan kaki Anda terasa nyaman dan sejuk.</p>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="<?= base_url() ?>assets/images/detail/denah-akane.jpg" class="img-fluid" alt="">
+                                    <h4 class="carousel-title">Denah Bangunan Type Akane</h4>
+                                    <div class="carousel-subtitle">Harga mulai 1M</div>
+                                    <p class="carousel-text">Tipe Akane adalah rumah minimalis modern 2 lantai tipe 95 dengan 3 kamar tidur, dilengkapi dengan berbagai material premium dengan standar kualitas Jepang, seperti material HomogeneousTile Double Layer Alice yang digunakan sebagai penutup lantai sehingga membuat setiap injakan kaki Anda terasa nyaman dan sejuk.</p>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="<?= base_url() ?>assets/images/detail/denah-akane.jpg" class="img-fluid" alt="">
+                                    <h4 class="carousel-title">Denah Bangunan Type Akane</h4>
+                                    <div class="carousel-subtitle">Harga mulai 1M</div>
+                                    <p class="carousel-text">Tipe Akane adalah rumah minimalis modern 2 lantai tipe 95 dengan 3 kamar tidur, dilengkapi dengan berbagai material premium dengan standar kualitas Jepang, seperti material HomogeneousTile Double Layer Alice yang digunakan sebagai penutup lantai sehingga membuat setiap injakan kaki Anda terasa nyaman dan sejuk.</p>
+                                </div>
+                            </div>
+                            <ol class="bottom-indicators carousel-indicators">
+                                <li data-target="#banner-top-page" data-slide-to="0" class="active"></li>
+                                <li data-target="#banner-top-page" data-slide-to="1"></li>
+                                <li data-target="#banner-top-page" data-slide-to="2"></li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row my-3">
+            <div class="col-lg">
+                <img src="<?php echo base_url(); ?>assets/images/promo/interior-1.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg">
+                <img src="<?php echo base_url(); ?>assets/images/promo/interior-2.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg">
+                <img src="<?php echo base_url(); ?>assets/images/promo/interior-3.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg">
+                <img src="<?php echo base_url(); ?>assets/images/promo/interior-4.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg">
+                <img src="<?php echo base_url(); ?>assets/images/promo/interior-5.jpg" class="img-fluid" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="banner banner-bottom">
+        <div class="container d-flex">
+            <img src="<?php echo base_url(); ?>assets/images/promo/whatsapp-gradient.png" class="img-fluid img-icon">
+            <div class="d-flex align-items-center justify-content-between w-100">
+                <p class="banner-text">Dapatkan Pricelist & Brosur <br/>Green Bamboo Terrace via Whatsapp</p>
+                <button class="btn btn-hubungi-kami">Hubungi Kami</button>
+            </div>
+        </div>
+          
+    </div>
+    <div class="footer-promo">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <iframe src="https://maps.google.com/maps?q=green%20bamboo%20terrace&t=&z=11&ie=UTF8&iwloc=&output=embed" width="100%" height="100%" style="min-height: 500px;" allowfullscreen="" loading="lazy" class="footer-map"></iframe>
+                </div>
+                <div class="col-md-6">
+                    <h4 class="footer-promo-title">Lokasi Strategis</h4>
+                    <div class="footer-promo-item">
+                        <div class="footer-item-icon">
+                            <img src="<?php echo base_url(); ?>assets/images/promo/highway.svg" alt="">
+                        </div>
+                        <p class="mb-0">Akses Pintu Tol Jagorawi dan Tol Bocimi</p>
+                    </div>
+                    <div class="footer-promo-item">
+                        <div class="footer-item-icon">
+                            <img src="<?php echo base_url(); ?>assets/images/promo/train.svg" alt="">
+                        </div>
+                        <p class="mb-0">Dekat dengan Stasiun Bogor</p>
+                    </div>
+                    <div class="footer-promo-item">
+                        <div class="footer-item-icon">
+                            <img src="<?php echo base_url(); ?>assets/images/promo/shopping-bag.svg" alt="">
+                        </div>
+                        <p class="mb-0">Dekat dengan Mall Boxies Bogor</p>
+                    </div>
+
+                    <div class="footer-contact-wrapper">
+                        <div class="d-flex align-items-center mb-2">
+                            <div class="marker-icon">
+                                <img src="<?php echo base_url(); ?>assets/images/promo/pin.svg" alt="">
+                            </div>
+                            <div class="footer-contact-title">GREEN BAMBOO TERRACE</div>
+                        </div>
+                        <h4 class="font-weight-bold">Marketing Gallery</h4>
+                        <p>Jalan Raya Tajur No.30, Tajur - Kota Bogor 16141 <br/>
+                        Jawa Barat, Indonesia</p>
+                        <hr class="footer-contact-separator">
+                        <div class="footer-contact-info">Phone 0811 1403 280 | Phone 0251-8574 255 |Email bambootajur@gmail.com</div>
+                    </div>
+                </div>
+                <div class="col-md-12 text-center my-4">
+                    © Copyright 2022. All Rights Reserved | Green Bamboo Terrace, Development by Kebun Bambu Jakarta
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script defer src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script>
+
+</script>
